ce0008d60552ba64c9d5b942503bdc31bec35d8f
L-Alharbi <l.alharbi@acg.edu>
2024-07-21 16:56:15 +0300

    added product,login and register

--- application/application/settings.py
+++ application/application/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
+
 ]
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,6 +126,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = BASE_DIR / "static/images"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- application/store/views.py
+++ application/store/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm 
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
-def store(request):
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, passsword=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            None
+
     context = {}
+    return render(request, 'store/login.html', context)
+
+
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'store/register.html', context)
+
+
+
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products' :products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
